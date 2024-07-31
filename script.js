c95f0da73e206f73b9c92407589fdb2d5814bff0
AlessandroPFreitas <164437174+AlessandroPFreitas@users.noreply.github.com>
2024-07-31 12:07:14 -0600

    Projeto finalizado

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+const html = document.documentElement
+html.classList.toggle("light")
+
+// pegar tag img
+const img = document.querySelector("#profile img")
+
+// Substituir a imagem
+if(html.classList.contains('light')){
+  // Se tiver light mode, adicionar a imagem light
+ img.setAttribute('src', './assets/avatar-light.png')
+ img.setAttribute('alt', "Foto do Mayk Brito sorrindo, usando óculos e com jaqueta preta, sem barba e fundo azul")
+ } else {
+  img.setAttribute("src", "./assets/avatar.png")
+img.setAttribute( "alt","Foto do Mayk Brito sorrindo, usando óculos e com camisa preta, barba e fundo amarelo"
+)
+}
+}
