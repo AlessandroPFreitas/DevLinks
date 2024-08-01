f525b360634e16328caff406dd3d9211d63cbdc0
AlessandroPFreitas <164437174+AlessandroPFreitas@users.noreply.github.com>
2024-08-01 14:30:04 -0600

    Alterações de preferencia

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ const img = document.querySelector("#profile img")
 // Substituir a imagem
 if(html.classList.contains('light')){
   // Se tiver light mode, adicionar a imagem light
- img.setAttribute('src', './assets/avatar-light.png')
+ img.setAttribute('src', './assets/light.png')
  img.setAttribute('alt', "Foto do Mayk Brito sorrindo, usando óculos e com jaqueta preta, sem barba e fundo azul")
  } else {
-  img.setAttribute("src", "./assets/avatar.png")
+  img.setAttribute("src", "./assets/padrão.png")
 img.setAttribute( "alt","Foto do Mayk Brito sorrindo, usando óculos e com camisa preta, barba e fundo amarelo"
 )
 }
